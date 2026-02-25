995ba85dd93580cbf2470ff1463b942fa5608fcc
coder-priyanka27 <priyankarambhad09@gmail.com>
2026-02-25 17:29:35 +0530

    Adding Publisher class and give PublisherId as ForeignKey to the Books table

--- my-books/Data/Models/Book.cs
+++ my-books/Data/Models/Book.cs
@@ -12,5 +12,9 @@
         public int? Rate { get; set; }
         public string CoverUrl { get; set; }
         public DateTime DateAdded { get; set; }
+
+        // Navigation properties
+        public int PublisherId { get; set; }
+        public Publisher Publisher { get; set; }
     }
 }
