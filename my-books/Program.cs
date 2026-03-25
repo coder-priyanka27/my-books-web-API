7c60067bc3e59a572d1aa6782c0e3672402b96f5
coder-priyanka27 <priyankarambhad09@gmail.com>
2026-03-25 16:22:39 +0530

    Configuring JWT in Program.cs

--- my-books/Program.cs
+++ my-books/Program.cs
@@ -1,9 +1,14 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc.Versioning;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
 using my_books.Data;
+using my_books.Data.Models;
 using my_books.Data.Services;
 using Serilog;
+using System.Configuration;
+using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
 try
@@ -30,9 +35,6 @@ builder.Host.UseSerilog(); // VERY IMPORTANT
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
-builder.Services.AddIdentity<IdentityUser, IdentityRole>().AddEntityFrameworkStores<AppDbContext>()
-    .AddDefaultTokenProviders();
-
 // Configure the Services
 builder.Services.AddTransient<BooksService>();
 builder.Services.AddTransient<AuthorsService>();
@@ -53,6 +55,34 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+//Add Identity
+builder.Services.AddIdentity<ApplicationUser, IdentityRole>()
+    .AddEntityFrameworkStores<AppDbContext>()
+    .AddDefaultTokenProviders();
+
+//Add Authentication
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+})
+//Add Bearer Token
+.AddJwtBearer(options =>
+{
+    options.SaveToken = true;
+    options.RequireHttpsMetadata = false;
+    options.TokenValidationParameters = new TokenValidationParameters()
+    {
+        ValidateIssuerSigningKey = true,
+        IssuerSigningKey = new SymmetricSecurityKey(Encoding.ASCII.GetBytes(Configuration["JWT:Secret"])),
+        ValidateIssuer = true,
+        ValidIssuer = Configuration["JWT:Issuer"],
+        ValidateAudience = true,
+        ValidAudience = Configuration["JWT:Audience"],
+    };
+});
 
 var app = builder.Build();
 
